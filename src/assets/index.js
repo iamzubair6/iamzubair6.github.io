4427f95e2cb3935d944646153cd78a539b82fcc2
ZUBAIR-RAHMAN <zubair.rahman89@gmail.com>
2023-05-13 23:58:53 +0600

    complete portolio wesite with all the components and assets

---
+++ src/assets/index.js
@@ -0,0 +1,67 @@
+import backend from './backend.png';
+import close from './close.svg';
+import creator from './creator.png';
+import github from './github.png';
+import githubProfie from './githubProfile.png';
+import linkedin from './linkedind.png';
+import live from './live.png';
+import logo from './logo.png';
+import menu from './menu.svg';
+import mobile from './mobile.png';
+import web from './web.png';
+
+import css from './tech/css.png';
+import docker from './tech/docker.png';
+import figma from './tech/figma.png';
+import git from './tech/git.png';
+import html from './tech/html.png';
+import javascript from './tech/javascript.png';
+import mongodb from './tech/mongodb.png';
+import nodejs from './tech/nodejs.png';
+import reactjs from './tech/reactjs.png';
+import redux from './tech/redux.png';
+import tailwind from './tech/tailwind.png';
+import threejs from './tech/threejs.svg';
+import typescript from './tech/typescript.png';
+
+import carrent from './carrent.png';
+import techsist from './company/techsist.png';
+import jobit from './jobit.png';
+import material from './tech/material.png';
+import next from './tech/next.png';
+import scss from './tech/scss.png';
+import tripguide from './tripguide.png';
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  carrent,
+  jobit,
+  tripguide,
+  material,
+  scss,
+  next,
+  techsist,
+  githubProfie,
+  linkedin,
+  live,
+};
