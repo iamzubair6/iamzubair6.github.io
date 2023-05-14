e3919ae4cf045616a886374251a1fe3765a1b035
ZUBAIR-RAHMAN <zubair.rahman89@gmail.com>
2023-05-14 23:32:40 +0600

    Skill section done

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -8,16 +8,18 @@ import { BallCanvas } from './canvas';
 
 const Tech = () => {
   return (
-    <div className="flex flex-row flex-wrap justify-center gap-10">
-      <motion.div variants={textVariant()}>
+    <>
+      <motion.div variants={textVariant()} style={{ marginBottom: '20px' }}>
         <h2 className={`${styles.sectionHeadText} text-center`}>Skills</h2>
       </motion.div>
-      {technologies.map((technology) => (
-        <div className="w-28 h-28" key={technology.name}>
-          <BallCanvas icon={technology.icon} />
-        </div>
-      ))}
-    </div>
+      <div className="flex flex-row flex-wrap justify-center gap-10">
+        {technologies.map((technology) => (
+          <div className="w-28 h-28" key={technology.name}>
+            <BallCanvas icon={technology.icon} />
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
