d6e1de0b93f9586240f027e1090478280f3a6170
iamzubair6 <zubair.rahman89@gmail.com>
2023-08-24 11:46:44 +0600

    constants: new project added

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,42 +1,45 @@
-import backend from './backend.png';
-import close from './close.svg';
-import creator from './creator.png';
-import github from './github.png';
-import githubProfie from './githubProfile.png';
-import linkedin from './linkedind.png';
-import live from './live.png';
-import logo from './logo.png';
-import menu from './menu.svg';
-import mobile from './mobile.png';
-import web from './web.png';
+import backend from "./backend.png";
+import close from "./close.svg";
+import creator from "./creator.png";
+import github from "./github.png";
+import githubProfie from "./githubProfile.png";
+import linkedin from "./linkedind.png";
+import live from "./live.png";
+import logo from "./logo.png";
+import menu from "./menu.svg";
+import mobile from "./mobile.png";
+import web from "./web.png";
 
-import css from './tech/css.png';
-import docker from './tech/docker.png';
-import figma from './tech/figma.png';
-import git from './tech/git.png';
-import html from './tech/html.png';
-import javascript from './tech/javascript.png';
-import mongodb from './tech/mongodb.png';
-import nodejs from './tech/nodejs.png';
-import reactjs from './tech/reactjs.png';
-import redux from './tech/redux.png';
-import tailwind from './tech/tailwind.png';
-import threejs from './tech/threejs.svg';
-import typescript from './tech/typescript.png';
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import threejs from "./tech/threejs.svg";
+import typescript from "./tech/typescript.png";
 
-import image1 from './Screenshot_1.png';
-import image2 from './Screenshot_2.png';
-import image3 from './Screenshot_3.png';
-import image4 from './Screenshot_4.png';
-import image5 from './Screenshot_5.png';
-import image6 from './Screenshot_6.png';
-import image7 from './Screenshot_7.png';
+import image1 from "./Screenshot_1.png";
+import image2 from "./Screenshot_2.png";
+import image3 from "./Screenshot_3.png";
+import image4 from "./Screenshot_4.png";
+import image5 from "./Screenshot_5.png";
+import image6 from "./Screenshot_6.png";
+import image7 from "./Screenshot_7.png";
+import image8 from "./Screenshot_8.png";
+import image9 from "./Screenshot_9.png";
+import image10 from "./Screenshot_10.png";
 
-import techsist from './company//techsist.png';
+import techsist from "./company//techsist.png";
 
-import material from './tech/material.png';
-import next from './tech/next.png';
-import scss from './tech/scss.png';
+import material from "./tech/material.png";
+import next from "./tech/next.png";
+import scss from "./tech/scss.png";
 
 export {
   logo,
@@ -74,4 +77,7 @@ export {
   image5,
   image6,
   image7,
+  image8,
+  image9,
+  image10,
 };
--- src/constants/index.js
+++ src/constants/index.js
@@ -4,12 +4,15 @@ import {
   git,
   html,
   image1,
+  image10,
   image2,
   image3,
   image4,
   image5,
   image6,
   image7,
+  image8,
+  image9,
   javascript,
   material,
   mongodb,
@@ -20,57 +23,57 @@ import {
   techsist,
   threejs,
   web,
-} from '../assets';
+} from "../assets";
 
 export const navLinks = [
   {
-    id: 'about',
-    title: 'About',
+    id: "about",
+    title: "About",
   },
   {
-    id: 'work',
-    title: 'Work',
+    id: "work",
+    title: "Work",
   },
   {
-    id: 'contact',
-    title: 'Contact',
+    id: "contact",
+    title: "Contact",
   },
 ];
 
 const services = [
   {
-    title: 'Web Developer',
+    title: "Web Developer",
     icon: web,
   },
 ];
 
 const technologies = [
   {
-    name: 'HTML 5',
+    name: "HTML 5",
     icon: html,
   },
   {
-    name: 'CSS 3',
+    name: "CSS 3",
     icon: css,
   },
   {
-    name: 'JavaScript',
+    name: "JavaScript",
     icon: javascript,
   },
   {
-    name: 'Material UI',
+    name: "Material UI",
     icon: material,
   },
   {
-    name: 'React JS',
+    name: "React JS",
     icon: reactjs,
   },
   {
-    name: 'Next js',
+    name: "Next js",
     icon: next,
   },
   {
-    name: 'Tailwind CSS',
+    name: "Tailwind CSS",
     icon: tailwind,
   },
   // {
@@ -78,39 +81,39 @@ const technologies = [
   //   icon: nodejs,
   // },
   {
-    name: 'MongoDB',
+    name: "MongoDB",
     icon: mongodb,
   },
   {
-    name: 'Three JS',
+    name: "Three JS",
     icon: threejs,
   },
   {
-    name: 'git',
+    name: "git",
     icon: git,
   },
   {
-    name: 'figma',
+    name: "figma",
     icon: figma,
   },
   {
-    name: 'SCSS',
+    name: "SCSS",
     icon: scss,
   },
 ];
 
 const experiences = [
   {
-    title: 'Jr. Frontend Developer',
-    company_name: 'Techsist Ltd',
+    title: "Jr. Frontend Developer",
+    company_name: "Techsist Ltd",
     icon: techsist,
-    iconBg: '#383E56',
-    date: 'Novemeber 2022 - Present',
+    iconBg: "#383E56",
+    date: "Novemeber 2022 - Present",
     points: [
-      'Developing and maintaining web applications using React.js and other related technologies.',
-      'Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
-      'Implementing responsive design and ensuring cross-browser compatibility.',
-      'Participating in code reviews and providing constructive feedback to other developers.',
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
     ],
   },
 ];
@@ -144,194 +147,279 @@ const experiences = [
 
 const projects = [
   {
-    name: 'Notun Asha',
-    description: 'Online news portal of daily notun asha.',
+    name: "Notun Asha",
+    description: "Online news portal of daily notun asha.",
     tags: [
       {
-        name: 'techsist-ltd',
-        color: 'pink-text-gradient',
+        name: "techsist-ltd",
+        color: "pink-text-gradient",
       },
       {
-        name: 'react',
-        color: 'blue-text-gradient',
+        name: "react",
+        color: "blue-text-gradient",
       },
       {
-        name: 'material-ui',
-        color: 'green-text-gradient',
+        name: "material-ui",
+        color: "green-text-gradient",
       },
       {
-        name: 'rest-api',
-        color: 'pink-text-gradient',
+        name: "rest-api",
+        color: "pink-text-gradient",
       },
       {
-        name: 'react-quary',
-        color: 'blue-text-gradient',
+        name: "react-quary",
+        color: "blue-text-gradient",
       },
     ],
     image: image2,
-    source_code_link: '',
-    live_link: 'https://dailynotunasha.com/',
+    source_code_link: "",
+    live_link: "https://dailynotunasha.com/",
   },
   {
-    name: 'Mesmerize: Express Your Love',
-    description: 'E-commerce site for customized gifts and more',
+    name: "Mesmerize: Express Your Love",
+    description: "E-commerce site for customized gifts and more",
     tags: [
       {
-        name: 'techsist ltd',
-        color: 'pink-text-gradient',
+        name: "techsist ltd",
+        color: "pink-text-gradient",
       },
       {
-        name: 'react',
-        color: 'blue-text-gradient',
+        name: "react",
+        color: "blue-text-gradient",
       },
       {
-        name: 'rest-api',
-        color: 'green-text-gradient',
+        name: "rest-api",
+        color: "green-text-gradient",
       },
       {
-        name: 'material-Ui',
-        color: 'pink-text-gradient',
+        name: "material-Ui",
+        color: "pink-text-gradient",
       },
       {
-        name: 'react-quary',
-        color: 'blue-text-gradient',
+        name: "react-quary",
+        color: "blue-text-gradient",
       },
     ],
     image: image1,
-    source_code_link: '',
-    live_link: 'https://mesmerizebd.com/',
+    source_code_link: "",
+    live_link: "https://mesmerizebd.com/",
   },
   {
-    name: 'Multiverse-Madness',
-    description: 'This project is about virtual reality.',
+    name: "XYZ Engine",
+    description:
+      "This project is about data parse and calculation from CSV file. User can upload CSV file that have X,Y,Z row, after uploading file internal calculation will happen and user will get max and min values of X,Y,Z. By uploading CSV file user can see chart",
     tags: [
       {
-        name: 'nextjs',
-        color: 'blue-text-gradient',
+        name: "react js",
+        color: "blue-text-gradient",
       },
       {
-        name: 'framer-motion',
-        color: 'green-text-gradient',
+        name: "Material-ui",
+        color: "green-text-gradient",
       },
       {
-        name: 'telwind',
-        color: 'pink-text-gradient',
+        name: "react router",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "chart js",
+        color: "green-text-gradient",
+      },
+    ],
+    image: image8,
+    source_code_link: "https://github.com/iamzubair6/xyz-engine",
+    live_link: "https://xyz-battery-engine.vercel.app/",
+  },
+  {
+    name: "Multiverse-Madness",
+    description: "This project is about virtual reality.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "framer-motion",
+        color: "green-text-gradient",
+      },
+      {
+        name: "telwind",
+        color: "pink-text-gradient",
       },
     ],
     image: image5,
-    source_code_link: 'https://github.com/iamzubair6/multiverse-madness-nextjs',
-    live_link: 'https://multiverse-madness-nextjs.vercel.app/',
+    source_code_link: "https://github.com/iamzubair6/multiverse-madness-nextjs",
+    live_link: "https://multiverse-madness-nextjs.vercel.app/",
   },
   {
-    name: 'Downtownbd',
-    description: 'This project is based on resturent menu fully responsive.',
+    name: "Corber | Dashboard",
+    description:
+      "This is complete dashboard system for manageing medicine and crops data. Here admin can upload corps image with complete details and crops disease. Admin can also uload and manage medicine data. This dashboad have create, edit, delete option with full dynamic state",
     tags: [
       {
-        name: 'react',
-        color: 'blue-text-gradient',
+        name: "react js",
+        color: "pink-text-gradient",
       },
       {
-        name: 'material-ui',
-        color: 'green-text-gradient',
+        name: "material-ui",
+        color: "blue-text-gradient",
       },
       {
-        name: 'vite',
-        color: 'pink-text-gradient',
+        name: "rest api",
+        color: "green-text-gradient",
+      },
+      {
+        name: "react quary",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "vite",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: image9,
+    source_code_link: "",
+    live_link: "",
+  },
+  {
+    name: "Edstructure",
+    description: "Edstructure is a complete learning management system.",
+    tags: [
+      {
+        name: "react js",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "SCSS",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Bootstrap",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "rest api",
+        color: "green-text-gradient",
+      },
+      {
+        name: "react quary",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: image10,
+    source_code_link: "",
+    live_link: "https://edstructure.com/home",
+  },
+  {
+    name: "Downtownbd",
+    description: "This project is based on resturent menu fully responsive.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "material-ui",
+        color: "green-text-gradient",
+      },
+      {
+        name: "vite",
+        color: "pink-text-gradient",
       },
     ],
     image: image7,
-    source_code_link: 'https://github.com/iamzubair6/Simple-menu-vite',
-    live_link: 'https://www.downtownbd.xyz/',
+    source_code_link: "https://github.com/iamzubair6/Simple-menu-vite",
+    live_link: "https://www.downtownbd.xyz/",
   },
   {
-    name: 'Mesmerize Dashboard',
+    name: "Mesmerize Dashboard",
     description:
-      'A full featured dashboard for managing Mesmerize admin panel.',
+      "A full featured dashboard for managing Mesmerize admin panel.",
     tags: [
       {
-        name: 'react',
-        color: 'blue-text-gradient',
+        name: "react",
+        color: "blue-text-gradient",
       },
       {
-        name: 'material-ui',
-        color: 'green-text-gradient',
+        name: "material-ui",
+        color: "green-text-gradient",
       },
       {
-        name: 'vite',
-        color: 'pink-text-gradient',
+        name: "vite",
+        color: "pink-text-gradient",
       },
       {
-        name: 'react-quary',
-        color: 'green-text-gradient',
+        name: "react-quary",
+        color: "green-text-gradient",
       },
       {
-        name: 'rest-api',
-        color: 'blue-text-gradient',
+        name: "rest-api",
+        color: "blue-text-gradient",
       },
     ],
     image: image3,
-    source_code_link: '',
-    live_link: '',
+    source_code_link: "",
+    live_link: "",
   },
   {
-    name: 'Personal portfolio',
+    name: "Personal portfolio",
     description:
-      'This is my personal portfolio website for showcasing my work.',
+      "This is my personal portfolio website for showcasing my work.",
     tags: [
       {
-        name: 'react',
-        color: 'blue-text-gradient',
+        name: "react",
+        color: "blue-text-gradient",
       },
       {
-        name: 'telwind',
-        color: 'green-text-gradient',
+        name: "telwind",
+        color: "green-text-gradient",
       },
       {
-        name: 'vite',
-        color: 'pink-text-gradient',
+        name: "vite",
+        color: "pink-text-gradient",
       },
       {
-        name: 'framer-motion',
-        color: 'green-text-gradient',
+        name: "framer-motion",
+        color: "green-text-gradient",
       },
     ],
     image: image4,
-    source_code_link: 'https://github.com/iamzubair6/Portfolio',
-    live_link: '',
+    source_code_link: "https://github.com/iamzubair6/Portfolio",
+    live_link: "",
   },
   {
-    name: 'Edstructure Dashboard',
-    description: 'A complete education management system',
+    name: "Edstructure Dashboard",
+    description: "A complete education management system",
     tags: [
       {
-        name: 'react',
-        color: 'blue-text-gradient',
+        name: "react",
+        color: "blue-text-gradient",
       },
       {
-        name: 'Bootstrap',
-        color: 'green-text-gradient',
+        name: "Bootstrap",
+        color: "green-text-gradient",
       },
       {
-        name: 'vite',
-        color: 'pink-text-gradient',
+        name: "vite",
+        color: "pink-text-gradient",
       },
       {
-        name: 'rest-api',
-        color: 'green-text-gradient',
+        name: "rest-api",
+        color: "green-text-gradient",
       },
       {
-        name: 'scss',
-        color: 'green-text-gradient',
+        name: "scss",
+        color: "green-text-gradient",
       },
       {
-        name: 'react-quary',
-        color: 'green-text-gradient',
+        name: "react-quary",
+        color: "green-text-gradient",
       },
     ],
     image: image6,
-    source_code_link: '',
-    live_link: '',
+    source_code_link: "",
+    live_link: "",
   },
 ];
 
-export { services, technologies, experiences, projects };
+export { experiences, projects, services, technologies };
